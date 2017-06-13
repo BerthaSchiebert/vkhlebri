a2acaf7c009d9c1b135152fade2cf55dbbbcbf2f
BerthaSchiebert <shirinova.valeria@yandex.ru>
2017-06-13 17:54:55 +0300

    haat main

--- js/script.js
+++ js/script.js
@@ -1,2 +1,21 @@
 $(document).ready(function(){
+
+	$(".owl-carousel").owlCarousel({
+		items: 1,
+		nav: true,
+		navText: "",
+		loop: true
+	});
+	
+});
+
+$(window).scroll(function() {
+
+	if($(this).scrollTop() >= 300) {
+		$(".header-top2").addClass("header-visible");
+	}
+	else{
+		$(".header-top2").removeClass("header-visible");
+	}
+
 });
